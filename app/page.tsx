dec2fec5bd236147d91cf3e093a58a6b69223fc9
djd <206951504+ditt-dev@users.noreply.github.com>
2025-04-16 11:16:18 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 'use client';
 
+import Card from '@/lib/classes';
 import { round1, round2, round3, round4, winMessage } from '@/lib/utils';
 
 import Image from 'next/image';
-import { Button, Box, Container, Typography } from '@mui/material';
+import { Button, Box, Container, Grid, Item, Typography } from '@mui/material';
 import cardBack from '@/public/backs/red2.svg';
 
 export default function Home() {
-	round1(0);
-	round2(0);
-	round3(0);
-	round4(0);
-	winMessage();
+	// round1(0);
+	// round2(0);
+	// round3(0);
+	// round4(0);
+	// winMessage();
+
+	const history: Card[] = [];
 
 	return (
 		<Container
@@ -21,15 +24,28 @@ export default function Home() {
 				backgroundColor: 'blue',
 				display: 'flex',
 				flexDirection: 'column',
-				height: '100vh',
 			}}
 		>
-			<Typography>hello from page.tsx</Typography>
-			{/* <Typography variant={'h1'}>Ride the Bus</Typography>
+			<Typography variant={'h1'}>Ride the Bus</Typography>
 
-			<Image alt={'test'} src={cardBack} />
+			{/* Cards */}
+			<Box>
+				<Grid container spacing={6}>
+					{history.map(i => {
+						return (
+							<Grid key={i} size={6}>
+								<Image alt={'card back'} priority src={cardBack} />
+							</Grid>
+						);
+					})}
+				</Grid>
+			</Box>
 
-			<Button>Begin game</Button> */}
+			{/* Message */}
+			<Box></Box>
+
+			{/* Buttons */}
+			<Box></Box>
 		</Container>
 	);
 }

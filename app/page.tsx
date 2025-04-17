f0b8d344a28722b1e031a4deb719e0d1cf1a75dc
djd <206951504+ditt-dev@users.noreply.github.com>
2025-04-17 12:27:04 -0400

    Update page.tsx layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,23 @@ import Card from '@/lib/classes';
 import { round1, round2, round3, round4, winMessage } from '@/lib/utils';
 
 import Image from 'next/image';
-import { Button, Box, Container, Grid, Item, Typography } from '@mui/material';
+import { Button, Box, Container, Grid, Stack, Typography } from '@mui/material';
 import cardBack from '@/public/backs/red2.svg';
 
 export default function Home() {
+	const headerHeight = 64;
+
 	// round1(0);
 	// round2(0);
 	// round3(0);
 	// round4(0);
 	// winMessage();
+
+	const data = [
+		{ id: 0, name: 'child0' },
+		{ id: 1, name: 'child1' },
+		{ id: 2, name: 'child2' },
+	];
 
 	const history: Card[] = [];
 
@@ -20,32 +28,38 @@ export default function Home() {
 		<Container
 			maxWidth={'md'}
 			sx={{
-				alignItems: 'center',
 				backgroundColor: 'blue',
-				display: 'flex',
-				flexDirection: 'column',
+				height: `calc(100vh - ${headerHeight}px)`,
 			}}
 		>
-			<Typography variant={'h1'}>Ride the Bus</Typography>
+			<Typography
+				variant={'h1'}
+				sx={{ display: 'flex', justifyContent: 'center' }}
+			>
+				Ride the Bus
+			</Typography>
 
-			{/* Cards */}
-			<Box>
-				<Grid container spacing={6}>
-					{history.map(i => {
-						return (
-							<Grid key={i} size={6}>
-								<Image alt={'card back'} priority src={cardBack} />
-							</Grid>
-						);
-					})}
-				</Grid>
-			</Box>
-
-			{/* Message */}
-			<Box></Box>
-
-			{/* Buttons */}
-			<Box></Box>
+			<Stack spacing={2} sx={{ width: '100%' }}>
+				{data.map(child => {
+					return (
+						<Box key={child.id} sx={{ flex: 1, outline: 'solid' }}>
+							{child.name}
+						</Box>
+					);
+				})}
+			</Stack>
 		</Container>
 	);
 }
+
+const Child0 = function (name) {
+	return <h1>{name}</h1>;
+};
+
+const Child1 = function (name) {
+	return <h1>{name}</h1>;
+};
+
+const Child2 = function (name) {
+	return <h1>{name}</h1>;
+};

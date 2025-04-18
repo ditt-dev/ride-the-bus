bda17133ec1fa192c7a3d81bed4d629ad5583c8d
djd <206951504+ditt-dev@users.noreply.github.com>
2025-04-18 08:55:33 -0400

    Update page.tsx layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,12 @@ import Image from 'next/image';
 import { Button, Box, Container, Grid, Stack, Typography } from '@mui/material';
 import cardBack from '@/public/backs/red2.svg';
 
+import { useTheme } from '@mui/material/styles';
+
 export default function Home() {
 	const headerHeight = 64;
+
+	const theme = useTheme();
 
 	// round1(0);
 	// round2(0);
@@ -16,13 +20,7 @@ export default function Home() {
 	// round4(0);
 	// winMessage();
 
-	const data = [
-		{ id: 0, name: 'child0' },
-		{ id: 1, name: 'child1' },
-		{ id: 2, name: 'child2' },
-	];
-
-	const history: Card[] = [];
+	const history = ['card0', 'card1', 2, 3];
 
 	return (
 		<Container
@@ -39,27 +37,68 @@ export default function Home() {
 				Ride the Bus
 			</Typography>
 
-			<Stack spacing={2} sx={{ width: '100%' }}>
-				{data.map(child => {
-					return (
-						<Box key={child.id} sx={{ flex: 1, outline: 'solid' }}>
-							{child.name}
-						</Box>
-					);
-				})}
+			<Stack sx={{ width: '100%' }}>
+				{/* Cards container */}
+				<Box sx={{ height: '33vh', outline: 'solid' }}>
+					<Grid container spacing={2} sx={{ height: '100%' }}>
+						{/* Display history from rounds 1 and 2 */}
+						<Grid size={2}>
+							<Stack sx={{ height: '100%' }}>
+								{[0, 1].map(historyIdx => {
+									return (
+										<Box
+											key={historyIdx}
+											sx={{
+												alignItems: 'center',
+												display: 'flex',
+												flex: 2,
+												justifyContent: 'center',
+												marginBottom: `${
+													historyIdx === 0 ? theme.spacing(1) : ''
+												}`,
+												marginTop: `${
+													historyIdx === 1 ? theme.spacing(1) : ''
+												}`,
+												outline: 'solid',
+											}}
+										>
+											{history.at(historyIdx)}
+										</Box>
+									);
+								})}
+							</Stack>
+						</Grid>
+
+						<Grid
+							size={5}
+							sx={{
+								alignItems: 'center',
+								display: 'flex',
+								justifyContent: 'center',
+								outline: 'solid',
+							}}
+						>
+							lg
+						</Grid>
+						<Grid
+							size={5}
+							sx={{
+								alignItems: 'center',
+								display: 'flex',
+								justifyContent: 'center',
+								outline: 'solid',
+							}}
+						>
+							lg
+						</Grid>
+					</Grid>
+				</Box>
+
+				{/* Message container */}
+				<Box></Box>
+				{/* Buttons container */}
+				<Box></Box>
 			</Stack>
 		</Container>
 	);
 }
-
-const Child0 = function (name) {
-	return <h1>{name}</h1>;
-};
-
-const Child1 = function (name) {
-	return <h1>{name}</h1>;
-};
-
-const Child2 = function (name) {
-	return <h1>{name}</h1>;
-};

b46b18add83c85e1dd92292a36f29c630b4af774
djd <206951504+ditt-dev@users.noreply.github.com>
2025-04-22 11:16:38 -0400

    Update CardsContainer.tsx

--- components/CardsContainer.tsx
+++ components/CardsContainer.tsx
@@ -1,6 +1,8 @@
 import { CardBox } from '@/components/index';
-import { Divider, Stack } from '@mui/material';
+import { Box, Divider, Stack } from '@mui/material';
 import { useState } from 'react';
+
+import Image from 'next/image';
 
 import card0 from '@/public/faces/0.svg';
 import card10 from '@/public/faces/10.svg';
@@ -10,45 +12,93 @@ import cardBack from '@/public/backs/red2.svg';
 
 export default function CardsContainer() {
 	const history = [cardBack, card0, card10, card20, card30];
+	// const history = [];
 
-	const [round, setRound] = useState(3);
+	const [round, setRound] = useState(1);
 
-	return (
-		<Stack
-			direction={'row'}
-			divider={<Divider orientation={'vertical'} />}
-			spacing={2}
-			sx={{ height: '100%' }}
-		>
-			{/* Display history from rounds 1 and 2 */}
-			<Stack divider={<Divider variant={'middle'} />} sx={{ flex: 1 }}>
-				{[1, 2].map(historyIdx => {
-					return (
-						<CardBox
-							key={historyIdx}
-							name={'test'}
-							src={history.at(historyIdx)}
-						/>
-					);
-				})}
-			</Stack>
+	switch (round) {
+		case 3:
+			return (
+				<Stack
+					direction={'row'}
+					divider={<Divider orientation={'vertical'} />}
+					spacing={2}
+					sx={{ height: '100%' }}
+				>
+					{/* Display history from rounds 1 and 2 */}
+					<Stack divider={<Divider variant={'middle'} />} sx={{ flex: 1 }}>
+						{[1, 2].map(historyIdx => {
+							return (
+								<CardBox
+									key={historyIdx}
+									name={'test'}
+									src={history.at(historyIdx)}
+								/>
+							);
+						})}
+					</Stack>
 
-			{/* Display history from rounds 3 and 4 */}
-			<Stack
-				direction={'row'}
-				divider={<Divider orientation={'vertical'} />}
-				sx={{ flex: 3 }}
-			>
-				{[3, 4].map(historyIdx => {
-					return (
-						<CardBox
-							key={historyIdx}
-							name={'test'}
-							src={history.at(historyIdx)}
-						/>
-					);
-				})}
-			</Stack>
-		</Stack>
-	);
+					{/* Display history from rounds 3 and 4 */}
+					<Stack
+						direction={'row'}
+						divider={<Divider orientation={'vertical'} />}
+						sx={{ flex: 3 }}
+					>
+						{[3, 4].map(historyIdx => {
+							return (
+								<CardBox
+									key={historyIdx}
+									name={'test'}
+									src={history.at(historyIdx)}
+								/>
+							);
+						})}
+					</Stack>
+				</Stack>
+			);
+		default:
+			return (
+				<Box
+					sx={{
+						height: '100%',
+						// width: '100%',
+						// position: 'absolute',
+						outline: 'solid green 5px',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Box
+						sx={{
+							transform: 'rotate(10deg)',
+							outline: 'solid orange',
+							height: '100%',
+						}}
+					>
+						{[-2, -1, 0, 1].map(i => {
+							return (
+								<Box
+									key={i}
+									sx={{
+										background: 'white',
+										height: '200px',
+										width: '150px',
+										borderRadius: '5px',
+										position: 'absolute',
+										boxShadow: '0 0 30px rgba(0, 0, 0, 0.8)',
+										left: '50%',
+										// top: ' 50%',
+										transform: `rotate(${15 * i}deg) 
+									// translate(-50%, -50%)
+									`,
+										transformOrigin: 'center 200%',
+									}}
+								/>
+							);
+						})}
+					</Box>
+				</Box>
+			);
+	}
 }

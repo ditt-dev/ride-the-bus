c2650b25b74d337da8ec672693338785c0b07604
djd <206951504+ditt-dev@users.noreply.github.com>
2025-04-28 10:42:43 -0400

    Improve responsive behavior on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,21 @@ import card0 from '@/public/faces/0.svg';
 import Card from '@/lib/classes';
 import { round1, round2, round3, round4, winMessage } from '@/lib/utils';
 
-import { Button, Box, Container, Stack, Typography } from '@mui/material';
+import {
+	Button,
+	Box,
+	Container,
+	Stack,
+	Typography,
+	useMediaQuery,
+} from '@mui/material';
 
 import { useTheme } from '@mui/material/styles';
 
 export default function Home() {
 	const theme = useTheme();
 
-	const headerHeight = 64;
+	const headerHeight = useMediaQuery(theme.breakpoints.down('sm')) ? 56 : 64;
 
 	// round1(0);
 	// round2(0);

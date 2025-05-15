0f7936446f2704ec14861ae2698a17c03aa34534
djd <206951504+ditt-dev@users.noreply.github.com>
2025-05-15 07:59:05 -0400

    Tidy page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { CardsContainer } from '@/components';
 import Image from 'next/image';
 import card0 from '@/public/faces/0.svg';
 
-import Card from '@/lib/classes';
+// import Card from '@/lib/classes';
 // import { round1, round2, round3, round4, winMessage } from '@/lib/utils';
 
 import {

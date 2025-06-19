b985a0e69a8234b6d433e263bbbb0416f47b8ff1
djd <206951504+ditt-dev@users.noreply.github.com>
2025-06-19 07:20:46 -0400

    Update imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { CardsContainer } from '@/components';
 
 import Image from 'next/image';
-// import card0 from '@/public/faces/0.svg';
+import card0 from '@/public/faces/0.svg';
 
 import Card from '@/lib/classes';
 import { round1, round2, round3, round4, winMessage } from '@/lib/utils';
